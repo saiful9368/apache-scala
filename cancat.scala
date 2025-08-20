75ebc7c2d1d43434c058cd416d8c27fcfd43abab
Saiful Rahman <114799928+saiful9368@users.noreply.github.com>
2025-08-20 13:49:25 +0530

    Create cancat.scala

---
+++ cancat.scala
@@ -0,0 +1,25 @@
+scala> :paste
+// Entering paste mode (ctrl-D to finish)
+
+import scala.io.StdIn.readLine
+
+def concat(a: String, b: String): String = {
+  val z = a + " " + b
+  z
+}
+
+val a = readLine("Enter first string: ")
+val b = readLine("Enter second string: ")
+
+println(concat(a, b))
+println(concat(a, b).length())
+
+
+// Exiting paste mode, now interpreting.
+
+saiful  Rahhmanring: Rahhman
+15
+import scala.io.StdIn.readLine
+def concat(a: String, b: String): String
+val a: String = "saiful "
+val b: String = Rahhman
