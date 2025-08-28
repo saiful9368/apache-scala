c51e8ddab9e0df78287318f71bc5176e94b14cfe
Saiful Rahman <114799928+saiful9368@users.noreply.github.com>
2025-08-28 10:55:31 +0530

    Update arrays.scala

--- arrays.scala
+++ arrays.scala
@@ -24,3 +24,22 @@ scala> arr1.foreach(println)
 2
 3
 4
+
+
+scala> val c=Array.fill(2)("Hello")
+val c: Array[String] = Array(Hello, Hello)
+
+scala> val d="Hello".toArray
+val d: Array[Char] = Array(H, e, l, l, o)
+
+scala> var ar1=Array.range(0,11)
+var ar1: Array[Int] = Array(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+
+scala> var ar2=ar1.filter(_%2==0)
+var ar2: Array[Int] = Array(0, 2, 4, 6, 8, 10)
+
+scala> var ar3=ar2.map(_*2)
+var ar3: Array[Int] = Array(0, 4, 8, 12, 16, 20)
+
+scala> var ar4=ar3.reverse
+var ar4: Array[Int] = Array(20, 16, 12, 8, 4, 0)
